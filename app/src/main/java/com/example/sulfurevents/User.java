7ff2d9741c9a5c99255493b6bbd968d227b7c951
aarmandhal <adhal@ualberta.ca>
2025-11-03 16:01:58 -0700

    add home

---
+++ app/src/main/java/com/example/sulfurevents/User.java
@@ -0,0 +1,61 @@
+package com.example.sulfurevents;
+
+public class User {
+    private String deviceId;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private boolean isAdmin;
+
+    public User(String deviceId, String name, String email, String phoneNumber, boolean isAdmin){
+        this.deviceId = deviceId;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.isAdmin = isAdmin;
+    }
+
+    public User() {
+        this.isAdmin = false;
+    }
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+}
